74e85c1e305c52ad1c79d4c9b009dfeb858d9aec
Lê Đức Thịnh <le.duc.thinh.24081994@gmail.com>
2015-12-03 17:06:59 +0700

    t

--- src/testgit/thinh.java
+++ src/testgit/thinh.java
@@ -3,9 +3,13 @@ package testgit;
 public class thinh {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 
 		System.out.println("thanh nhat");
 
+=======
+		System.out.println("le duc thinh");
+>>>>>>> parent of 875abca... nhat
 
 	}
 
